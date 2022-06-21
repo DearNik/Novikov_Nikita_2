5a11db54d712dcc7c802e2f569c90c5df2e01329
DearNik <nikitanovikov25@gmail.com>
2022-06-21 09:56:53 +0300

    Laba 2
    Laba 2

---
+++ Лаба 2/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Лаба_2
+{
+
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string file;
+
+                Console.WriteLine("Загрузка времени из файла или из устройства - f/d");
+                string f = Console.ReadLine();
+                if (f == "f")
+                {
+                    file = Convert.ToString(Deserialize());
+
+                    bool timer = true;
+
+                    while (timer == true)
+                    {
+                        try
+                        {
+                            Console.WriteLine(file);
+
+                            int hc = 0;
+                            int mc = 0;
+                            int sc = 0;
+
+                            Time Time1 = new Time(file);
+
+                            Time1.TimeChoise(out hc, out mc, out sc);
+
+                            if (mc < 10 && sc < 10) { Console.WriteLine($"{hc}:0{mc}:0{sc}"); }
+                            else if (sc < 10) { Console.WriteLine($"{hc}:{mc}:0{sc}"); }
+                            else if (mc < 10) { Console.WriteLine($"{hc}:0{mc}:{sc}"); }
+                            else { Console.WriteLine($"{hc}:{mc}:{sc}"); }
+
+                            Console.ReadKey();
+                            Console.Clear();
+                            timer = false;
+
+                            Serialize($"{hc}:{mc}:{sc}");
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Ошибка ввода");
+                            Console.ReadKey();
+                            Console.Clear();
+                        }
+                    }
+
+
+                }
+                else if (f == "d")
+                {
+                    file = null;
+                    bool timer = true;
+
+                    while (timer == true)
+                    {
+                        try
+                        {
+                            DateTime time = DateTime.Now;
+
+                            string format = "HH:mm:ss";
+
+
+
+                            Console.WriteLine(time.ToString(format));
+
+                            int hc = 0;
+                            int mc = 0;
+                            int sc = 0;
+
+                            Time Time1 = new Time(file);
+
+                            Time1.TimeChoise(out hc, out mc, out sc);
+
+                            string resulttime
+                                ;
+                            if (mc < 10 && sc < 10)
+                            {
+                                resulttime = $"{hc}:0{mc}:0{sc}";
+                                Console.WriteLine($"{hc}:0{mc}:0{sc}");
+                            }
+                            else if (sc < 10)
+                            {
+                                resulttime = $"{hc}:{mc}:0{sc}";
+                                Console.WriteLine($"{hc}:{mc}:0{sc}");
+                            }
+                            else if (mc < 10)
+                            {
+                                resulttime = $"{hc}:0{mc}:{sc}";
+                                Console.WriteLine($"{hc}:0{mc}:{sc}");
+                            }
+                            else
+                            {
+                                resulttime = $"{hc}:{mc}:{sc}";
+                                Console.WriteLine($"{hc}:{mc}:{sc}");
+                            }
+
+                            Serialize(resulttime);
+                            Console.ReadKey();
+                            Console.Clear();
+                            timer = false;
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Ошибка ввода");
+                            Console.ReadKey();
+                            Console.Clear();
+                        }
+                    }
+                }
+                static object Deserialize()
+                {
+                    var serializer = new JsonSerializer();
+
+                    using (var sw = new StreamReader("D:\\Институт\\Прога\\Лаба 2\\time read.json"))
+                    using (var reader = new JsonTextReader(sw))
+                    {
+                        return serializer.Deserialize(reader);
+                    }
+                }
+                static void Serialize(object obj)
+                {
+                    using (StreamWriter file = File.CreateText("D:\\Институт\\Прога\\Лаба 2\\time wright.json"))
+                    {
+                        JsonSerializer serializer = new JsonSerializer();
+                        serializer.Serialize(file, obj);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Лаба 2/Time.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Лаба_2
+{
+    public class Time
+    {
+        string file;
+        public int hc;
+        public int mc;
+        public int sc;
+        public Time(string file)
+        {
+            this.file = file;
+        }
+        public void TimeChoise(out int hc, out int mc, out int sc)
+        {
+            if(file == null)
+            {
+                DateTime time = DateTime.Now;
+
+                string format = "HH:mm:ss";
+
+                string[] mystring = time.ToString(format).Split(':');
+
+                var TimeList = new List<int>();
+
+                for (int j = 0; j < mystring.Length; j++)
+                {
+                    TimeList.Add(Convert.ToInt32(mystring[j]));
+                }
+
+                Console.WriteLine("Изменить время - h/m/s");
+
+                string pick = Console.ReadLine();
+
+                int hc1 = 0;
+                int mc1 = 0;
+                int sc1 = 0;
+
+                if (pick == "h")
+                {
+                    int h = Convert.ToInt32(Console.ReadLine());
+                    if (h < 24)
+                    {
+                        hc1 = h - TimeList[0];
+                    }
+                    else
+                    {
+                        hc1 = (h % 24) - TimeList[0];
+                    }
+                }
+                if (pick == "m")
+                {
+                    int m = Convert.ToInt32(Console.ReadLine());
+                    if (m < 60)
+                    {
+                        mc1 = m - TimeList[1];
+                    }
+                    else
+                    {
+                        mc1 = (m % 60) - TimeList[1];
+                        hc1 = m / 60;
+                        if (hc1 + TimeList[0] > 24)
+                        {
+                            hc1 = hc1 % 24;
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                }
+                if (pick == "s")
+                {
+                    int s = Convert.ToInt32(Console.ReadLine());
+                    if (s < 60)
+                    {
+                        sc1 = s - TimeList[2];
+
+                    }
+                    else
+                    {
+                        sc1 = (s % 60) - TimeList[2];
+                        mc1 = s / 60;
+                        if (mc1 + TimeList[0] > 60)
+                        {
+                            hc1 = mc1 / 60;
+                            if (hc1 + TimeList[0] > 24)
+                            {
+                                hc1 = hc1 % 24;
+                            }
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                }
+
+
+                hc = hc1 + TimeList[0];
+                mc = mc1 + TimeList[1];
+                sc = sc1 + TimeList[2];
+            }
+            else
+            {
+                string[] mystring = file.Split(':');
+
+                var TimeList = new List<int>();
+
+                for (int j = 0; j < mystring.Length; j++)
+                {
+                    TimeList.Add(Convert.ToInt32(mystring[j]));
+                }
+
+                Console.WriteLine("Изменить время - h/m/s");
+
+                string pick = Console.ReadLine();
+
+                int hc1 = 0;
+                int mc1 = 0;
+                int sc1 = 0;
+
+                if (pick == "h")
+                {
+                    int h = Convert.ToInt32(Console.ReadLine());
+                    if (h < 24)
+                    {
+                        hc1 = h - TimeList[0];
+                    }
+                    else
+                    {
+                        hc1 = (h % 24) - TimeList[0];
+                    }
+                }
+                if (pick == "m")
+                {
+                    int m = Convert.ToInt32(Console.ReadLine());
+                    if (m < 60)
+                    {
+                        mc1 = m - TimeList[1];
+                    }
+                    else
+                    {
+                        mc1 = (m % 60) - TimeList[1];
+                        hc1 = m / 60;
+                        if (hc1 + TimeList[0] > 24)
+                        {
+                            hc1 = hc1 % 24;
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                }
+                if (pick == "s")
+                {
+                    int s = Convert.ToInt32(Console.ReadLine());
+                    if (s < 60)
+                    {
+                        sc1 = s - TimeList[2];
+
+                    }
+                    else
+                    {
+                        sc1 = (s % 60) - TimeList[2];
+                        mc1 = s / 60;
+                        if (mc1 + TimeList[0] > 60)
+                        {
+                            hc1 = mc1 / 60;
+                            if (hc1 + TimeList[0] > 24)
+                            {
+                                hc1 = hc1 % 24;
+                            }
+                        }
+                        else
+                        {
+
+                        }
+                    }
+                }
+
+
+                hc = hc1 + TimeList[0];
+                mc = mc1 + TimeList[1];
+                sc = sc1 + TimeList[2];
+            }
+
+        }
+
+    }
+}
